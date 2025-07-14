dd40e367d3003feb645d6b22a18bcd051fa3ea86
maulida <maulida.nuzulia@dagangan.com>
2025-07-14 20:23:12 +0700

    day01-variables, constans, strings and numbers

---
+++ 100-days-of-swiftUI/day01-intro-swift-ui/store-whole-numbers.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+let score = 10
+let reallyBig = 100000000
+let reallyBigN = 100_000_000
+let reallyBigNumb = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+
